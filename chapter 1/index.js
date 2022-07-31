41228f6b1c8cfedb1120d65fe38b8470a6e76174
Programador cero <pr0gr4m4d0rc3r0@gmail.com>
2022-07-31 16:26:08 -0500

    capitulo 1

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -15,3 +15,14 @@ Node.js, uno de los principios más evangelizados es diseñar módulos (y paquet
 
 Tener módulos más pequeños y más enfocados una sola tarea permite a todos compartir o reutilizar incluso el código más pequeño; es el principio Don't Repeat Yourself (DRY) aplicado a un nivel completamente nuevo.
 */
+
+
+//Pequeña superficie
+/*
+Realizar modulos con solo las funcionalidades necesarias y en el mejor de los casos que sean minimas las funcionalidades, tambien prohibir la extensión o implementación de más funcionalidades, no dejar que ningun elemento que no sea necesario sea expuesto al exterior.
+*/
+
+//Simplicidad y pragmatismo
+/*
+¿Alguna vez has oído hablar del principio Keep It Simple, Stupid (KISS)? (en español mantenerlo simple y estupido) Richard P. Gabriel,
+*/
